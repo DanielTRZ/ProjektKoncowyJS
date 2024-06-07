37da39ae37d07a01b1be2a50af3f2ab3dd8b37ef
Daniel Trzeciński <71105883+DanielTRZ@users.noreply.github.com>
2024-06-07 12:37:53 +0200

    Update task2.js

--- tasks/task2.js
+++ tasks/task2.js
@@ -2,7 +2,7 @@
 
 function wieksza() {
 const liczba1 = parseFloat (document.getElementById("firstNumberToCompare").value);
-    const liczba2 = parseFloat (document.getElementById("secondNumberToCompare").value);
+const liczba2 = parseFloat (document.getElementById("secondNumberToCompare").value);
     const wynik = document.getElementById('resultContainer2');
     
     if(liczba1 === liczba2) 
