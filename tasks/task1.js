1e6d37cf3b803b811208acdf79b2d1d8230f84d7
Daniel Trzeciński <71105883+DanielTRZ@users.noreply.github.com>
2024-06-07 12:34:11 +0200

    Update task1.js

--- tasks/task1.js
+++ tasks/task1.js
@@ -3,7 +3,7 @@ const luckyNumber = "7";
 const num = document.getElementById('randomNumber').value;  
 const wynik = document.getElementById('resultContainer1'); 
     
-    if (num === luckyNumber)  
+if (num === luckyNumber)  
          wynik.innerHTML = '<span style="color:green">Gratulacje wygrałeś.</span>';  
     else 
          wynik.innerHTML = '<span style="color.red">Niestety przegrałeś</span>';
