dd36c3d020a538ec49995e8510b32a87e9c987c2
Daniel Trzeciński <71105883+DanielTRZ@users.noreply.github.com>
2024-06-08 04:06:04 +0200

    Update task3.js

--- tasks/task3.js
+++ tasks/task3.js
@@ -13,8 +13,6 @@ if(num > 10) suma += num;
 wynik.innerHTML = `<span>Suma liczb wiekszych od 10 w tablicy wynosi ${suma}`;
 }
 // Zadanie 4
-
-
 function dodaj(){
     const nowaLiczba = parseInt(numberToAddToArray.value);
     
