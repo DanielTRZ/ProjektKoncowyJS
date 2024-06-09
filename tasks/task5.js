52b4de48fbba76701286146bcdf8b304cf6319c4
Daniel Trzeciński <71105883+DanielTRZ@users.noreply.github.com>
2024-06-09 13:34:16 +0200

    Update task5.js

--- tasks/task5.js
+++ tasks/task5.js
@@ -5,7 +5,7 @@ for(i=0; i<5;i++){
 let element = document.createElement('div');
 element.id = 'bloczek';
 element.style.width = '100px';
-        element.style.height = '100px';
+element.style.height = '100px';
         element.style.backgroundColor = getRandomColor();
         var cont = document.getElementById("squaresContainer");
         cont.appendChild(element);}           
